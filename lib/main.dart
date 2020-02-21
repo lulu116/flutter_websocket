dff46a06db1a10d8d930108f2f3556ae2273cb01
lulu116 <sliusting970@163.com>
2020-02-21 16:18:11 +0800

    git code

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: WebSocketRoute(),
+    );
+  }
+}
+
+class WebSocketRoute extends StatefulWidget {
+  @override
+  _WebSocketRouteState createState() => new _WebSocketRouteState();
+}
+
+class _WebSocketRouteState extends State<WebSocketRoute> {
+  TextEditingController _controller = new TextEditingController();
+  IOWebSocketChannel channel;
+  String _text = "";
+
+
+  @override
+  void initState() {
+    //创建websocket连接
+    channel = new IOWebSocketChannel.connect('ws://echo.websocket.org');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("WebSocket(内容回显)"),
+      ),
+      body: new Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Form(
+              child: new TextFormField(
+                controller: _controller,
+                decoration: new InputDecoration(labelText: 'Send a message'),
+              ),
+            ),
+            new StreamBuilder(
+              stream: channel.stream,
+              builder: (context, snapshot) {
+                //网络不通会走到这
+                if (snapshot.hasError) {
+                  _text = "网络不通...";
+                } else if (snapshot.hasData) {
+                  _text = "echo: "+snapshot.data;
+                }
+                return new Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  child: new Text(_text),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _sendMessage,
+        tooltip: 'Send message',
+        child: new Icon(Icons.send),
+      ),
+    );
+  }
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      channel.sink.add(_controller.text);
+    }
+  }
+
+  @override
+  void dispose() {
+    channel.sink.close();
+    super.dispose();
+  }
+}
